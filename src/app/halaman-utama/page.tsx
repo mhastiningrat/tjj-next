4a71a720debdd194ba6089c3224115c4a38c2491
mhastin <mhastiningrat@gmail.com>
2025-01-28 00:48:25 +0700

    fixing/props type and page title

--- src/app/halaman-utama/page.tsx
+++ src/app/halaman-utama/page.tsx
@@ -14,11 +14,11 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 // import Dashboard from "../dashboard/page"
-import { ReactNode } from "react"
+import { ReactElement } from "react"
 // import QualitasIntake from "../qualitas-intake/pages"
 // import { DoubleBarChart } from "../sales-chart/pages"
 
-export default function Sidebar({children,page}: { children: ReactNode,page:string }) {
+export default function Sidebar({children,page}: { children: ReactElement,page:string }) {
   return (
     <SidebarProvider>
       <AppSidebar />
